c928a956ce565044fdca01c5a67660ee5d3bac75
unknown <lopesraonny@gmail.com>
2024-10-16 15:47:32 -0300

    condicional1

---
+++ script.js
@@ -0,0 +1,16 @@
+let nome = prompt("Qual o seu nome?");
+//alert ("Seja bem vindo(a), " + nome)
+// let idade = prompt("Quantos anos você tem?")
+// if (idade >= 18) {
+//         alert ("Você já pode tirar carteira de motorista!!!")
+// } else{
+//     alert ("Você ainda não tem idade para dirigir")
+// }
+
+
+let gostardepizza = prompt("Você gosta de pizza? responda (sim ou não)")
+if (gostardepizza === "sim") {
+    alert ("Que ótimo! Vamos comer juntos")
+} else{
+    alert ("Tudo bem, a gente pode comer outra coisa")
+}
